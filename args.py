87f286dec1f3af5feba5dd105af364545b1e08a1
Daniel Sarfati <daniel.sarfati@lcecorp.com>
2021-11-09 08:46:49 -0500

    updated args to show only args passed in, not $0

--- args.py
+++ args.py
@@ -6,3 +6,8 @@ import sys
 
 #dir(sys)
 print(f"USAGE: {sys.base_prefix}: arg1 arg2")
+print(f"You entered {len(sys.argv) -1 } args")
+if len(sys.argv) > 1: 
+    #for loop to provide index number and value (idx, val) and using enumerate function in python. This time it is starting from 1 as specified by [1:]
+    for idx, val in enumerate(sys.argv[1:]):
+        print(f"for arg number {idx}, you entered {val}")
